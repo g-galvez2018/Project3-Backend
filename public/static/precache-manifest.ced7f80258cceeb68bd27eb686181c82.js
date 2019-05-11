bdfe2445d61f25638d7802698d19d7087e602dad
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-05-11 14:12:05 -0400

    Front End Update

---
+++ public/static/precache-manifest.ced7f80258cceeb68bd27eb686181c82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda66dabf1526ff00687a657b968ecb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7a62975d0aff2461bd",
+    "url": "/static/css/main.51a47420.chunk.css"
+  },
+  {
+    "revision": "f0678b2292c9d6b605fb",
+    "url": "/static/js/2.d06facea.chunk.js"
+  },
+  {
+    "revision": "bf7a62975d0aff2461bd",
+    "url": "/static/js/main.6d131f16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
